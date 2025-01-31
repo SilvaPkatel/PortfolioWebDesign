2602397cfb6c35c29960a3d43737fcd2b7a4c0bb
SilvaPkatel <133651555+SilvaPkatel@users.noreply.github.com>
2025-01-31 20:31:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// ---------------- animation typing ----------------------
+
+var typed = new Typed(".typing", {
+  strings: [
+    "Web Designer",
+    "Graphiste",
+    "Infographiste",
+    "UX Designer",
+    "UI Designer",
+    "Motion Designer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 100,
+  loop: true,
+});
+
+// ---------------- Aside Menu & Page web ----------------------
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    removeBackSection();
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        addBackSection(j);
+        // allSection[j].classList.add("back-section");
+      }
+
+      navList[j].querySelector("a").classList.remove("active");
+    }
+
+    this.classList.add("active");
+    showSelection(this);
+
+    if (window.innerWidth < 1200) {
+      asideSectionToggerBtn();
+    }
+  });
+}
+
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section");
+  }
+}
+
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
+}
+
+function showSelection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
+
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+    if (
+      target ===
+      navList[i].querySelector("a").getAttribute("href").split("#")[1]
+    ) {
+      navList[i].querySelector("a").classList.add("active");
+    }
+  }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
+  showSelection(this);
+  updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
+  console.log("hello");
+});
+
+// ---------------- Menu burger ----------------------
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionToggerBtn();
+});
+function asideSectionToggerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
+}
+
+// -------------------- Animation Page portfolio ---------------------
